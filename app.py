14a82632f4fd1adee97e8ee8428eb56822792508
toipapo <michaeljoy.rodriguez@g.msuiit.edu.ph>
2022-01-22 14:56:48 +0800

    hiding credentials with dotenv

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL, MySQLdb
+from dotenv import load_dotenv
+import os
 import cloudinary
 import cloudinary.uploader
 
-
+load_dotenv()
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'SSIS'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_HOST'] = os.getenv("DB_HOST")
+app.config['MYSQL_USER'] = os.getenv("DB_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("DB_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("DB_DB")
+app.config['MYSQL_CURSORCLASS'] = os.getenv("DB_CURSORCLASS")
 
 cloudinary.config(
-    cloud_name = 'dglj9023b',
-    api_key = '115467398695897',
-    api_secret = '3tS8qm3H5BdgSmbfmvkdTKotAlk'
+    cloud_name = os.getenv('cloud_name'),
+    api_key = os.getenv('api_key'),
+    api_secret = os.getenv('api_secret')
 )
 
 mysql = MySQL(app)
